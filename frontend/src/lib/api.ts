9030d60179cf75b2597b1fd73665ba6c115fd24b
Dasakami <dendasakami@gmail.com>
2025-12-05 08:43:48 +0600

    add docker compose

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://147.45.108.120:8004';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
